2b5d41a62bd6d0bdf06a2ce25a10c6f3a5aa6dc5
Gourgette <gourgette.alexis.valynseele@gmail.com>
2020-10-01 23:55:44 +0200

    More learning + first mini project

---
+++ 03_navbar/js/app.js
@@ -0,0 +1,28 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        //Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        //Change the burger form
+        burger.classList.toggle('burger-toggle');
+
+        //Animate links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+        });
+    });
+
+}
+const main = () => {
+    navSlide();
+}
+
+main();
